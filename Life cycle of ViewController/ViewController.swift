2454c93524d6f21cfd356d18a2e2c71743ca1404
Mikhail Medvedev <smoke.mma@gmail.com>
2019-04-27 23:24:04 +0300

    Initial Commit

---
+++ Life cycle of ViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Life cycle of ViewController
+//
+//  Created by Михаил Медведев on 27/04/2019.
+//  Copyright © 2019 Mikhail Medvedev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
